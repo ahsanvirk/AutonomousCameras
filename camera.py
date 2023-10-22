8349cf79acad854a3df2e0ae39b891eea2da451d
gakOCC <144056588+gakOCC@users.noreply.github.com>
2023-10-22 11:03:27 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,9 +3,24 @@ import pytesseract
 import numpy as np
 from main import setCurrentBrakeForce, accelSpeed, setMaxSpeed, printCarInfo
 import re
+import threading
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
-pytesseract.pytesseract.tesseract_cmd = 'tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\gak\Desktop\New folder\tesseract.exe"
+
+def __draw_label(img, text, pos, bg_color):
+   font_face = cv2.FONT_HERSHEY_SIMPLEX
+   scale = 1.0
+   color = (255, 255, 255)
+   thickness = cv2.FILLED
+   margin = 2
+   txt_size = cv2.getTextSize(text, font_face, scale, thickness)
+
+   end_x = pos[0] + txt_size[0][0] + margin
+   end_y = pos[1] - txt_size[0][1] - margin
+
+   cv2.rectangle(img, pos, (end_x, end_y), bg_color, thickness)
+   cv2.putText(img, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
 
 while(True): 
 	ret, frame = vid.read()
@@ -13,9 +28,12 @@ while(True):
 	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 	data = cv2.CascadeClassifier(r"stopSign.xml")
+	faces = cv2.CascadeClassifier(r"test.xml")
 
 	found = data.detectMultiScale(gray, minSize=(20,20))
+	facesFound = faces.detectMultiScale(gray, minSize=(20,20))
 	amtFound = len(found)
+	amtFaces = len(facesFound)
 
 	speedSign = cv2.CascadeClassifier(r"speedLimit.xml")
 	foundSign = speedSign.detectMultiScale(gray, minSize=(20,20))
@@ -24,15 +42,23 @@ while(True):
 	if amtFound != 0:
 		for(x,y,width,height) in found:
 			cv2.rectangle(rgb, (x,y), (x+height, y+width), (0,255,0), 5)
-			setCurrentBrakeForce(1.0)
+			__draw_label(frame, 'Stop', (20,20), (0,0,0))
+			cv2.imshow('Frame', frame)
+   			# Display the resulting frame
+			#setCurrentBrakeForce(1.0)
+
+	if amtFaces != 0:
+		for(x,y,width,height) in facesFound:
+			cv2.rectangle(rgb, (x,y), (x+height, y+width), (255,5,3), 5)
 
 	if foundSignLen != 0:
 		text = pytesseract.image_to_string(gray, lang = 'eng', config = '--psm 6')
-		setMaxSpeed(re.findall(r'\d+', text))
-		print(re.findall(r'\d+', text))
+		for i in re.findall(r'\d+', text):
+			if(int(i) > 25 or int(i) % 5 == 0 or int(i) % 10 == 0):
+				if i != 0 and i > 15:
+					print(i)
 		
 	accelSpeed()
-	printCarInfo()
 
 		
 	
